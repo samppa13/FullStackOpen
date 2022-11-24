8a3385aa26b35302ec884a29e364c8a39a0ae086
samppa13 <samuelkosola@gmail.com>
2022-11-24 14:08:07 +0200

    2.2: kurssitiedot step7

--- Osa2/kurssitiedot/src/App.js
+++ Osa2/kurssitiedot/src/App.js
@@ -1,3 +1,14 @@
+const Total = ({ result }) => {
+  const sum = result.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, 0
+  )
+  return (
+    <p>
+      total of {sum} exercises
+    </p>
+  )
+}
+
 const Part = ({ part }) => {
   return (
     <p>
@@ -7,11 +18,13 @@ const Part = ({ part }) => {
 }
 
 const Content = ({ course }) => {
+  const result = course.parts.map(part => part.exercises)
   return (
     <div>
       {course.parts.map(part =>
         <Part key={part.id} part={part} />
         )}
+      <Total result={result} />
     </div>
   )
 }
